cec1a7da446e221188e4a6c5516aa62838d83a6f
George Kaimakis <geoptus@gmail.com>
2021-02-05 20:02:03 +0000

    Initial Commit:

---
+++ README.md
@@ -0,0 +1,11 @@
+## GeoCoordinates
+
+
+### Purpose
+These scripts extract position coordinates from Google Earth path objects (kml)
+and use these to generate formated gpx files for use with the
+[GPS Waypoint Finder App](https://play.google.com/store/apps)
+
+### Dependencies
+- _jq_ for processing json files.
+- _jc_ parcer for the parcing of the xml files
---
+++ parse_coords_template.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+file=$1
+if [ -z "$file" ]; then
+    echo "Usage: Provide a valid kml file." && exit
+fi
+
+
+folder=$2
+if [ -z "$folder" ]; then
+    echo "Usage: Provide a valid folder name." && exit
+fi
+
+
+name=$3
+if [ -z "$name" ]; then
+    echo "Usage: Provide a valid path name." && exit
+fi
+
+
+cat $file | jc --xml | jq -r ".kml.Document.Folder.Folder[] | select(.name ==
+\"${folder}\").Placemark[] | select(.name == \"${name}\").Polygon.outerBoundaryIs.LinearRing.coordinates" | tr ' ' '\n' | awk -F, '{print $2","$1}'
